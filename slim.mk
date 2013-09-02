cef185e4231ee8456a4830d383449ce074305248
7alvi <vishal29957@gmail.com>
2013-09-02 04:44:28 -0700

    Initialize

--- slim.mk
+++ slim.mk
@@ -1,14 +1,10 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 320
-TARGET_SCREEN_WIDTH  := 480
-
 # Include GSM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/htc/pico/overlay
 
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 # Inherit device configuration
@@ -18,10 +14,13 @@ PRODUCT_RELEASE_NAME := Explorer
 PRODUCT_VERSION_DEVICE_SPECIFIC := -Pico
 
 # Product name
-PRODUCT_NAME := cm_pico
+PRODUCT_NAME := slim_pico
 PRODUCT_BRAND := htc_europe
 PRODUCT_DEVICE := pico
 PRODUCT_MODEL := HTC Explorer A310e
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=pico BUILD_ID=JDQ39E BUILD_FINGERPRINT=htc_europe/htc_pico/pico:2.3.5/GRJ90/207463.1:user/release-keys
 PRIVATE_BUILD_DESC="1.28.401.1 CL207463 release-keys"
+
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
